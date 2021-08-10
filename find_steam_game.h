3c1dcfc27c915320a56729a80ff755a60f0c58d1
Cong <congusbongus@gmail.com>
2021-08-10 21:51:30 +1000

    Implement windows steam

--- find_steam_game.h
+++ find_steam_game.h
@@ -21,26 +21,86 @@ SOFTWARE.
 */
 #pragma once
 
+#include <tinydir.h>
+
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include <stdbool.h>
 
 #ifdef _MSC_VER
-# define _TINYDIR_FUNC static __inline
+#define _FSG_FUNC static __inline
 #elif !defined __STDC_VERSION__ || __STDC_VERSION__ < 199901L
-# define _TINYDIR_FUNC static __inline__
+#define _FSG_FUNC static __inline__
 #else
-# define _TINYDIR_FUNC static inline
+#define _FSG_FUNC static inline
 #endif
 
-_TINYDIR_FUNC
-bool fsg_game_exists(const char *name)
-{
-	(void)name;
+#ifdef _MSC_VER
+#include <windows.h>
+#include <winerror.h>
+#include <winreg.h>
+
+	static void _query_reg_key(
+		char *out, const HKEY key, const char *keypath, const char *valname)
+	{
+		HKEY pathkey;
+		DWORD pathtype;
+		DWORD pathlen;
+		LONG res;
+
+		if (ERROR_SUCCESS ==
+			RegOpenKeyEx(key, keypath, 0, KEY_QUERY_VALUE, &pathkey))
+		{
+			if (ERROR_SUCCESS ==
+					RegQueryValueEx(
+						pathkey, valname, 0, &pathtype, NULL, &pathlen) &&
+				pathtype == REG_SZ && pathlen != 0)
+			{
+				res = RegQueryValueEx(
+					pathkey, valname, 0, NULL, (LPBYTE)out, &pathlen);
+				if (res != ERROR_SUCCESS)
+				{
+					out[0] = '\0';
+				}
+			}
+			RegCloseKey(pathkey);
+		}
+	}
+#endif
+
+	_FSG_FUNC
+	bool fsg_game_exists(const char *name)
+	{
+#ifdef _MSC_VER
+		char buf[4096];
+		buf[0] = '\0';
+		_query_reg_key(
+			buf, HKEY_CURRENT_USER, "Software\\Valve\\Steam", "SteamPath");
+		if (strlen(buf) == 0)
+		{
+			_query_reg_key(
+				buf, HKEY_LOCAL_MACHINE, "Software\\Valve\\Steam",
+				"InstallPath");
+			if (strlen(buf) == 0)
+				return false;
+		}
+		strcat(buf, "/SteamApps/common/");
+		strcat(buf, name);
+
+		tinydir_dir dir;
+		if (tinydir_open(&dir, buf) != 0)
+		{
+			return false;
+		}
+		tinydir_close(&dir);
+		return true;
+#else
 	return false;
-}
+#endif
+	}
 
 #ifdef __cplusplus
 }
